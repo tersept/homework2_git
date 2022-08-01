79abeef21213e303edde4faeffec097e03a0bd6b
Irina <pashaginy@rambler.ru>
2022-08-01 13:27:59 +0300

    added test + refactoring

--- src/test/java/guru/qa/Git_test.java
+++ src/test/java/guru/qa/Git_test.java
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.Test;
 public class Git_test {
     @Test
     void firstTest(){
-        Assertions.assertTrue(3>1);
+        Assertions.assertFalse(3<1);
     }
     void secondTest(){
         Assertions.assertTrue(31>1);
+    }
+    void thirdTest(){
+        Assertions.assertFalse(4<6);
     }
 
 }
