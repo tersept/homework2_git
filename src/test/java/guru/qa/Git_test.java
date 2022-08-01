d3ab06bdc978110cf5479e0e2087d790f7c90d2a
Irina <pashaginy@rambler.ru>
2022-08-01 12:46:54 +0300

    first commit1

---
+++ src/test/java/guru/qa/Git_test.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Git_test {
+    @Test
+    void firstTest(){
+        Assertions.assertTrue(3>1);
+    }
+
+}
